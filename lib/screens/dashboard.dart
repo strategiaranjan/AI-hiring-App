ab6dafa85f928950fff0c20462e5f6e7950105d2
strategiaranjan <ranjan223116@gmail.com>
2025-04-08 07:42:20 +0530

    Create dashboard.dart

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Strategia Advizo Dashboard')),
+      body: Center(
+        child: Text('Welcome to Strategia Advizo AI Hiring App!'),
+      ),
+    );
+  }
+}
