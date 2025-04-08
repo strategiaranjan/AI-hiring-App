26ce472851cf472bb515665f0ce199bb05ee82d4
strategiaranjan <ranjan223116@gmail.com>
2025-04-08 07:41:24 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/dashboard.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Strategia Advizo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Dashboard(),
+    );
+  }
+}
